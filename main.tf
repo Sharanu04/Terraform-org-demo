3123cc4e7b49954ca1ec42ad9a8b19f7e62fa80d
Sharanu <Spdevops@test.com>
2025-11-22 16:18:10 +0530

    Update EC2 instance type to t3.small

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "aws_ami" "windows" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.windows.id
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
 
   vpc_security_group_ids = [module.vpc.default_security_group_id]
   subnet_id              = module.vpc.private_subnets[0]
